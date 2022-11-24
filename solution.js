6e85953cf0dda5d1d41fdb3922a4d6130e94f31e
Tmaria-net <118903617+Tmaria-net@users.noreply.github.com>
2022-11-24 10:41:53 +0300

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,24 @@
+\\Задание №1.1
+let message;
+let productPrice;
+message = "productName" + " " + "за" + " " + "productPrice" + " " + "теперь в корзине!"
+\\Конец решения задания №1.1
+
+
+\\Задание №1.2
+let newValue;
+let oldValue;
+newValue = oldValue;
+console.log(newValue + 1);
+
+\\Конец решения задания №1.2
+
+\\Задание №1.3.
+let newSum;
+let oldSum;
+let difference;
+let newSumText;
+newSum = oldSum + difference;
+\\newSumText = "newSum" + " ₽";
+console.log ("Сумма вашего заказа:", newSum, "рублей");
+\\Конец решения задания №1.3
